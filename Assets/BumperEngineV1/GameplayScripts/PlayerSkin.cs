29eee360a05dc38ddacf5ab1b9743e9ef6e494d1
Leclerc Christopher <leclerc.christopher@hotmail.com>
2026-09-14 15:19:14 +0200

    Commit #7

---
+++ Assets/BumperEngineV1/GameplayScripts/PlayerSkin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSkin : MonoBehaviour {
+
+    public Animator CharacterAnimator;
+    public PlayerBhysics Player;
+
+    public float skinRotationSpeed;
+
+    void Update()
+    {
+
+        CharacterAnimator.SetInteger("Action", 0);
+        CharacterAnimator.SetFloat("GroundSpeed", GetComponent<Rigidbody>().linearVelocity.magnitude);
+
+        Quaternion CharRot = Quaternion.LookRotation(GetComponent<Rigidbody>().linearVelocity, transform.up);  
+        CharacterAnimator.transform.rotation = Quaternion.Lerp(CharacterAnimator.transform.rotation, CharRot, Time.deltaTime * skinRotationSpeed);
+
+    }
+
+}
